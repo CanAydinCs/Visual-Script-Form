bcadab3fdd6e9e4b8fd46b27840145f3a4f5a274
Mehmet Can Aydın <mehmet1can1aydin@gmail.com>
2022-06-21 20:50:36 +0300

    a0.0.1
    Basic File Generation

--- Visual Script Forms/Form1.Designer.cs
+++ Visual Script Forms/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Visual_Script_Forms
 {
-    partial class Form1
+    partial class FormFileSelector
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnCreate = new System.Windows.Forms.Button();
+            this.BtnOpen = new System.Windows.Forms.Button();
+            this.BtnDir = new System.Windows.Forms.Button();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.fileSelector = new System.Windows.Forms.OpenFileDialog();
+            this.folderSelector = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCreate
+            // 
+            this.BtnCreate.Location = new System.Drawing.Point(148, 159);
+            this.BtnCreate.Name = "BtnCreate";
+            this.BtnCreate.Size = new System.Drawing.Size(94, 64);
+            this.BtnCreate.TabIndex = 0;
+            this.BtnCreate.Text = "Create";
+            this.BtnCreate.UseVisualStyleBackColor = true;
+            this.BtnCreate.Click += new System.EventHandler(this.BtnCreate_Click);
+            // 
+            // BtnOpen
+            // 
+            this.BtnOpen.Location = new System.Drawing.Point(749, 159);
+            this.BtnOpen.Name = "BtnOpen";
+            this.BtnOpen.Size = new System.Drawing.Size(94, 64);
+            this.BtnOpen.TabIndex = 1;
+            this.BtnOpen.Text = "Open";
+            this.BtnOpen.UseVisualStyleBackColor = true;
+            this.BtnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // BtnDir
+            // 
+            this.BtnDir.Location = new System.Drawing.Point(971, 113);
+            this.BtnDir.Name = "BtnDir";
+            this.BtnDir.Size = new System.Drawing.Size(94, 29);
+            this.BtnDir.TabIndex = 2;
+            this.BtnDir.Text = "...";
+            this.BtnDir.UseVisualStyleBackColor = true;
+            this.BtnDir.Click += new System.EventHandler(this.BtnDir_Click);
+            // 
+            // txtDir
+            // 
+            this.txtDir.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtDir.Location = new System.Drawing.Point(475, 115);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.ReadOnly = true;
+            this.txtDir.Size = new System.Drawing.Size(490, 27);
+            this.txtDir.TabIndex = 3;
+            // 
+            // fileSelector
+            // 
+            this.fileSelector.FileName = "New Visual Script Form";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(118, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 35);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Create New:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(659, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 35);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Open Existing:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Project Name:";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(118, 114);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.PlaceholderText = "New VSF Project";
+            this.txtFile.Size = new System.Drawing.Size(204, 27);
+            this.txtFile.TabIndex = 7;
+            // 
+            // FormFileSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1077, 246);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.BtnDir);
+            this.Controls.Add(this.BtnOpen);
+            this.Controls.Add(this.BtnCreate);
+            this.Name = "FormFileSelector";
+            this.Text = "Visual Script Forms";
+            this.Load += new System.EventHandler(this.FormFileSelector_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button BtnCreate;
+        private Button BtnOpen;
+        private Button BtnDir;
+        private TextBox txtDir;
+        private OpenFileDialog fileSelector;
+        private FolderBrowserDialog folderSelector;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtFile;
     }
 }
--- Visual Script Forms/Form1.cs
+++ Visual Script Forms/Form1.cs
@@ -1,10 +1,61 @@
 namespace Visual_Script_Forms
 {
-    public partial class Form1 : Form
+    public partial class FormFileSelector : Form
     {
-        public Form1()
+        public FormFileSelector()
         {
             InitializeComponent();
+        }
+
+        private void FormFileSelector_Load(object sender, EventArgs e)
+        {
+            fileSelector.InitialDirectory = Application.StartupPath;
+            fileSelector.Filter = "Visual Scripts Form files (*.vsf)|*.vsf";
+
+            folderSelector.SelectedPath = Application.StartupPath;
+        }
+
+        private void BtnCreate_Click(object sender, EventArgs e)
+        {
+            if(folderSelector.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = txtFile.Text == "" ? "New VSF Project" : txtFile.Text;
+
+                string currentPath = folderSelector.SelectedPath + @"/" + fileName;
+                string startFileName = currentPath + @"/" + fileName + ".vsf";
+
+                Directory.CreateDirectory(currentPath);
+                Directory.CreateDirectory(currentPath + @"/" + "workplaces");
+                FileStream file = File.Create(startFileName);
+                file.Close();
+
+                OpenProject(startFileName);
+            }
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            if (!txtDir.Text.EndsWith(".vsf"))
+            {
+                MessageBox.Show("Please select a form!");
+                return;
+            }
+
+            OpenProject(txtDir.Text);
+        }
+
+        private void BtnDir_Click(object sender, EventArgs e)
+        {
+            if (fileSelector.ShowDialog() == DialogResult.OK)
+            {
+                txtDir.Text = fileSelector.FileName;
+            }
+        }
+
+        private void OpenProject(string projectPath)
+        {
+            //TODO: fill this
+            MessageBox.Show(projectPath + " has opened!");
         }
     }
 }
